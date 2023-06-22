041726f410ba1f60b90b2034fd5a1ce0d5bb04de
mishraavinash98 <mishraavinash98@gmail.com>
2023-06-22 20:51:03 +0530

    Initial Commit

---
+++ src/CustomerApp/CustomerApp.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { CustomerComponent } from 
+'./CustomerApp.component';
+
+@NgModule({
+  declarations: [
+    CustomerComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [CustomerComponent]
+})
+export class CustomerModule { }
